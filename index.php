1df4d839da94fcb988a2dbc54cc91c1973e2cdcf
Siclaudio <siclaudioborges@gmail.com>
2017-05-04 10:12:10 -0300

    Update index.php

--- index.php
+++ index.php
@@ -4,6 +4,35 @@
 $curl = curl_init();
 
 curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://auth.exacttargetapis.com/v1/requestToken",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST",
+  CURLOPT_POSTFIELDS => "{\n    \"clientId\":\"685frwfn9jfmhm5av05g22r8\",\n    \"clientSecret\":\"A2zQCtNyAdg4OIwP5b5twGd0\"\n}",
+  CURLOPT_HTTPHEADER => array(
+    "content-type: application/json"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+<?php
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
   CURLOPT_URL => "https://www.exacttargetapis.com/hub/v1/campaigns",
   CURLOPT_RETURNTRANSFER => true,
   CURLOPT_ENCODING => "",
