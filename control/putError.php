07c6de3b711dc9c63ebcb87d0387bea4ed031f91
Siclaudio <siclaudioborges@gmail.com>
2017-05-04 11:46:05 -0300

    Update putError.php

--- control/putError.php
+++ control/putError.php
@@ -3,18 +3,18 @@ session_start("et@br");
 
 if(!isset($_SESSION['login']) || !isset($_SESSION['password'])){
     
-  header("Location: ../index.php");
+  header("Location:https://resultadospesquisasatisfacao.herokuapp.com/index.php");
 }
 if($_SESSION['permission'] != "admin"){
 	
-	header("Location: ../index.php");
+	header("Location: https://resultadospesquisasatisfacao.herokuapp.com/index.php");
 }	
 
 $id = $_GET['rule'];
 $error = $_GET['effort'];
 
-include "../class/Connection.class";
-include "../class/Activity.class";
+include "https://resultadospesquisasatisfacao.herokuapp.com/class/Connection.class";
+include "https://resultadospesquisasatisfacao.herokuapp.com/class/Activity.class";
 
 $job = new Activity();
 $job->setId($id);
