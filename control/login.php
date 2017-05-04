f6e9f2dd9dfb26d62b8f4f2a3bb84c3b9a5059f9
Siclaudio <siclaudioborges@gmail.com>
2017-05-04 11:41:41 -0300

    Update login.php

--- control/login.php
+++ control/login.php
@@ -6,8 +6,8 @@ $user = $_POST['user'];
 $password = $_POST['password'];
 $name = "Thiago";
 
-include "../class/Connection.class";
-include "../class/User.class";
+include "https://resultadospesquisasatisfacao.herokuapp.com/class/Connection.class";
+include "https://resultadospesquisasatisfacao.herokuapp.com/class/User.class";
 
 $validate = new User();
 $validate->setLogin($user);
@@ -26,11 +26,11 @@ if($password == $validate->getPassword()){
     echo $_SESSION['password'] = $validate->getPassword();
     echo $_SESSION['permission'] = $validate->getPermission();
 	
-    header("Location: ../pages/home.php?option=home");
+    header("Location: https://resultadospesquisasatisfacao.herokuapp.com/pages/home.php?option=home");
 }else{
     
     echo "<script text/javascript>alert('User or password invalid')</script>";
-    echo "<meta http-equiv='refresh' content='0;URL=../index.php'>";
+    echo "<meta http-equiv='refresh' content='0;URL=https://resultadospesquisasatisfacao.herokuapp.com/index.php'>";
 }
 
 ?>
