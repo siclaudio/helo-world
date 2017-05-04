936153848308e5be9805dc6b7b899bde6737d0c7
Siclaudio <siclaudioborges@gmail.com>
2017-05-04 10:36:05 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,28 +1,5 @@
 <?php
 
-$curl = curl_init();
+  echo "Helo heroku meu primeiro APP";
 
-curl_setopt_array($curl, array(
-  CURLOPT_URL => "https://www.exacttargetapis.com/hub/v1/campaigns",
-  CURLOPT_RETURNTRANSFER => true,
-  CURLOPT_ENCODING => "",
-  CURLOPT_MAXREDIRS => 10,
-  CURLOPT_TIMEOUT => 30,
-  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-  CURLOPT_CUSTOMREQUEST => "GET",
-  CURLOPT_HTTPHEADER => array(
-    "authorization: Bearer 7dVdKoFgYHGWGFgFd03EGmCX",
-    "content-type: application/json"
-  ),
-));
-
-$response = curl_exec($curl);
-$err = curl_error($curl);
-
-curl_close($curl);
-
-if ($err) {
-  echo "cURL Error #:" . $err;
-} else {
-  echo $response;
 }
